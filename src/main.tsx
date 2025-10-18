2b78fbb4f5df4e51e8eab26c92c7bfa6b3564126
RivysonClaudio <orivyson@gmail.com>
2025-10-18 09:33:24 -0300

    hot fix

--- src/main.tsx
+++ src/main.tsx
@@ -2,47 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Login from "./pages/Login.tsx";
-import Pesquisar from "./pages/Pesquisar.tsx";
-import DirecinamentoDash from "./pages/DirecinamentoDash.tsx";
-import DirecionamentoLista from "./pages/DirecionamentoList.tsx";
-import DirecionamentoView from "./pages/DirecionamentoView.tsx";
-import DirecionamentoForm from "./pages/DirecionamentoForm.tsx";
-
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <Login />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/admin",
-    element: <DirecinamentoDash />,
-  },
-  {
-    path: "/admin/direcionamento",
-    element: <DirecionamentoView />,
-  },
-  {
-    path: "/admin/direcionamento-lista",
-    element: <DirecionamentoLista />,
-  },
-  {
-    path: "/admin/direcionar",
-    element: <DirecionamentoForm />,
-  },
-  {
-    path: "/admin/direcionar/pesquisar",
-    element: <Pesquisar />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <h1>Hello World!</h1>
   </StrictMode>
 );
